130fed367f4c97b2931b6e3422c853da8311094f
jrobinfo <6076421+jrobinfo@users.noreply.github.com>
2023-12-07 00:38:45 -0500

    let accumulate data

--- ocean-websocket-client.js
+++ ocean-websocket-client.js
@@ -7,19 +7,32 @@ ws.on('open', function open() {
   ws.send(JSON.stringify({ "track-template": 0 }));
 });
 
+let accumulatedData = '';
+
 ws.on('message', function incoming(data) {
-  const dataStr = data.toString();
-  console.log('Received data:', dataStr);
+  accumulatedData += data.toString();
 
-  try {
-    const jsonData = JSON.parse(dataStr);
-    console.log('Parsed JSON data:', jsonData);
-
-    // Process the jsonData as needed
-  } catch (err) {
-    console.error('Error parsing JSON:', err);
+  // Check if the complete data is received (depends on the server's protocol)
+  if (isCompleteData(accumulatedData)) {
+    try {
+      const jsonData = JSON.parse(accumulatedData);
+      processJsonData(jsonData); // Process your JSON data here
+      accumulatedData = ''; // Reset for next message
+    } catch (err) {
+      console.error('Error parsing JSON:', err);
+    }
   }
 });
+
+function isCompleteData(dataString) {
+  // Implement logic to determine if the complete data has been received
+  // For example, check for a specific delimiter or end-of-data indicator
+}
+
+function processJsonData(jsonData) {
+  // Implement your data processing logic here
+}
+
 
 ws.on('error', function error(err) {
   console.error('WebSocket error:', err);
